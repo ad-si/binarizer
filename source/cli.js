fd5c91c3c4a0f23a6f2bfd40fc7eab423cef7ddd
Adrian Sieber <mail@adriansieber.com>
2017-02-08 21:26:20 +0000

    Fix formatting according to JavaScript styleguide

--- source/cli.js
+++ source/cli.js
@@ -1,20 +1,20 @@
 #! /usr/bin/env babel-node
 
-import binarizer from './index.js'
+const binarizer = require('..')
 
-let inputFilePath = process.argv[2]
-let outputFilePath = process.argv[3]
+const inputFilePath = process.argv[2]
+const outputFilePath = process.argv[3]
 
 
 if (!inputFilePath) {
-	console.log('Usage: binarize <path to input file> [path to output file]')
-	process.exit(1)
+  console.info('Usage: binarize <path to input file> [path to output file]')
+  process.exit(1)
 }
 
 binarizer
-	.setInputPath(inputFilePath)
-	.setOutputPath(outputFilePath)
-	.binarize()
-	.catch(error => {
-		throw error
-	})
+  .setInputPath(inputFilePath)
+  .setOutputPath(outputFilePath)
+  .binarize()
+  .catch(error => {
+    throw error
+  })
--- source/index.js
+++ source/index.js
@@ -1,100 +1,97 @@
-import path from 'path'
+const path = require('path')
 
-import gm from 'gm'
-import cv from 'opencv'
-import fsp from 'fs-promise'
+const cv = require('opencv')
+const fsp = require('fs-promise')
 
-let imageRegex = /^(.*)\.(jpg|png)$/i
+const imageRegex = /^(.*)\.(jpg|png)$/i
 
 
 export default class {
+  static convertAndSave (inputPath, outputPath) {
+    return new Promise((resolve, reject) => {
+      cv.readImage(inputPath, (error, image) => {
+        if (error) {
+          return reject(error)
+        }
+        if (image.width() < 1 || image.height() < 1) {
+          return reject(new Error('Image has no size'))
+        }
 
-	static convertAndSave (inputPath, outputPath) {
+        image.convertGrayscale()
+        image
+          .threshold(0, 255, 'Binary', 'Otsu')
+          .save(outputPath)
 
-		return new Promise((resolve, reject) => {
+        console.info(' ✔')
 
-			cv.readImage(inputPath, (error, image) => {
-				if (error)
-					return reject(error)
-
-				if (image.width() < 1 || image.height() < 1)
-					return reject(new Error('Image has no size'))
-
-				image.convertGrayscale()
-				image
-					.threshold(0, 255, 'Binary', 'Otsu')
-					.save(outputPath)
-
-				console.log(' ✔')
-
-				resolve(outputPath)
-			})
-		})
-	}
+        resolve(outputPath)
+      })
+    })
+  }
 
 
-	static setInputPath (path) {
-
-		if (typeof path !== 'string')
-			throw new TypeError(
-				`Path must be of type "string" instead of ${typeof path}`
-			)
-
-		this.inputPath = path
-		this.outputPath = this.outputPath || path.replace(
-			imageRegex,
-			'$1-bw.png'
-		)
-		return this
-	}
-
-
-	static setOutputPath (path) {
-
-		if (typeof path !== 'string' && typeof path !== 'undefined')
-			throw new TypeError(
-				`Path must be of type "string" instead of ${typeof path}`
-			)
-
-		this.outputPath = path || this.outputPath
-		return this
-	}
+  static setInputPath (inputPath) {
+    if (typeof inputPath !== 'string') {
+      throw new TypeError(
+        `Path must be of type "string" instead of ${typeof inputPath}`
+      )
+    }
+    this.inputPath = inputPath
+    this.outputPath = this.outputPath || inputPath.replace(
+      imageRegex,
+      '$1-bw.png'
+    )
+    return this
+  }
 
 
-	static binarize () {
+  static setOutputPath (outputPath) {
+    if (typeof outputPath !== 'string' && typeof outputPath !== 'undefined') {
+      throw new TypeError(
+        `Path must be of type "string" instead of ${typeof outputPath}`
+      )
+    }
+    this.outputPath = outputPath || this.outputPath
+    return this
+  }
 
-		function mapFileName (fileName) {
-			let outputFileName = fileName.replace(imageRegex, '$1-bw.png')
 
-			let inputFilePath = path.join(this.inputPath, fileName)
-			let outputFilePath = path.join(this.inputPath, outputFileName)
+  static binarize () {
 
-			process.stdout.write(path.join(
-				this.inputPath,
-				'{' + fileName + ' -> ' + outputFileName + '}'
-			))
+    function mapFileName (fileName) {
+      const outputFileName = fileName.replace(imageRegex, '$1-bw.png')
 
-			return this.convertAndSave(inputFilePath, outputFilePath)
-		}
+      const inputFilePath = path.join(this.inputPath, fileName)
+      const outputFilePath = path.join(this.inputPath, outputFileName)
 
-		return fsp
-			.stat(this.inputPath)
-			.then(stats => {
-				if (stats.isDirectory())
-					return fsp.readdir(this.inputPath)
-				else {
-					process.stdout.write(
-						this.inputPath + ' -> ' + this.outputPath
-					)
-					return this.convertAndSave(this.inputPath, this.outputPath)
-				}
-			})
-			.then(files => {
-				if (Array.isArray(files))
-					return Promise.all(files
-						.filter(fileName => imageRegex.test(fileName))
-						.map((fileName) => mapFileName.call(this, fileName))
-					)
-			})
-	}
+      process.stdout.write(path.join(
+        this.inputPath,
+        '{' + fileName + ' -> ' + outputFileName + '}'
+      ))
+
+      return this.convertAndSave(inputFilePath, outputFilePath)
+    }
+
+    return fsp
+      .stat(this.inputPath)
+      .then(stats => {
+        if (stats.isDirectory()) {
+          return fsp.readdir(this.inputPath)
+        }
+        else {
+          process.stdout.write(
+            this.inputPath + ' -> ' + this.outputPath
+          )
+          return this.convertAndSave(this.inputPath, this.outputPath)
+        }
+      })
+      .then(files => {
+        if (Array.isArray(files)) {
+          return Promise.all(files
+            .filter(fileName => imageRegex.test(fileName))
+            .map((fileName) => mapFileName.call(this, fileName))
+          )
+        }
+      })
+  }
 }
